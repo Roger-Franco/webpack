3c86bd9a0ac49d3f4bf5bfd7042e2a1864ea4024
Roger-Franco <rogerfrancogalt@gmail.com>
2022-05-29 23:12:58 -0400

    Adicionando WebServer

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-const modoDev = process.env.NODE_ENV !== 'production';
-const webpack = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const modoDev = process.env.NODE_ENV !== 'production'
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
   mode: modoDev ? 'development' : 'production',
@@ -11,18 +11,22 @@ module.exports = {
     filename: 'principal.js',
     path: __dirname + '/public'
   },
+  devServer: {
+    contentBase: "./public",
+    port: 9000
+  },
   optimization: {
     minimizer: [
       new UglifyJsPlugin({
         cache: true,
         parallel: true
       }),
-      new OptimizeCssAssetsPlugin({})
+      new OptimizeCSSAssetsPlugin({})
     ]
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'estilo.css',
+      filename: "estilo.css"
     })
   ],
   module: {
@@ -30,13 +34,13 @@ module.exports = {
       test: /\.s?[ac]ss$/,
       use: [
         MiniCssExtractPlugin.loader,
-        // 'style-loader', // Adicona CSS a DOM injetando a tag <style>
-        'css-loader', // interpreta @import, url() ...
-        'sass-loader'
+        // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+        'css-loader', // interpreta @import, url()...
+        'sass-loader',
       ]
     }, {
       test: /\.(png|svg|jpg|gif)$/,
-      use: ['file-loader'],
+      use: ['file-loader']
     }]
   }
 }
